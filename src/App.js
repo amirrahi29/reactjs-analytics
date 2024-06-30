a524ceaf32b292ed8e22b43e53fc2de9dc6c079a
Amir Rahi <amirrahi29@gmail.com>
2024-06-30 18:40:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics,logEvent } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbm77ycpu3WSjK26ZSyyqxBASNLseNtfk",
+  authDomain: "rojkharido-web-analytics.firebaseapp.com",
+  projectId: "rojkharido-web-analytics",
+  storageBucket: "rojkharido-web-analytics.appspot.com",
+  messagingSenderId: "835260043304",
+  appId: "1:835260043304:web:c93cd0b632abf7c303651d",
+  measurementId: "G-76DDWW9JMM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const App = () => {
+
+
+  const btnFunc1 = async()=>{
+    console.log("Helooo1");
+    logEvent(analytics,"btn1 Clicked");
+  }
+
+  const btnFunc2 = async()=>{
+    console.log("Helooo2");
+    logEvent(analytics,"btn2 Clicked");
+  }
+
+  const btnFunc3 = async()=>{
+    console.log("Helooo3");
+    logEvent(analytics,"btn3 Clicked");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   
+    
+    <>
+    
+    <h1 onClick={()=>btnFunc1()}>Click Button1</h1>
+    <h1 onClick={()=>btnFunc2()}>Click Button2</h1>
+    <h1 onClick={()=>btnFunc3()}>Click Button3</h1>
+    
+    </>
+
+  )
 }
 
-export default App;
+export default App
